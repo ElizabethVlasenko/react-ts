c89b8366f5b0e24edb90467409dbea41abc4b953
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-11-01 14:29:44 +0000

    Add a course goal component and define props type

---
+++ src/components/CourseGoal.tsx
@@ -0,0 +1,37 @@
+//import {type FC, type ReactNode } from "react"; //adding type will make it clear that this import can be removed for the browser
+import { type PropsWithChildren } from "react";
+
+// interface CourseGoalProps {
+//   title: string;
+//   children: ReactNode;
+// }
+
+type CourseGoalProps = PropsWithChildren<{ title: string }>;
+
+//FC - functional component
+
+// const CourseGoal: FC<CourseGoalProps> = ({ title, children }) => {
+//   return (
+//     <article>
+//       <div>
+//         <h2>{title}</h2>
+//         {children}
+//       </div>
+//       <button>Delete</button>
+//     </article>
+//   );
+// };
+
+// export default CourseGoal;
+
+export default function CourseGoal({ title, children }: CourseGoalProps) {
+  return (
+    <article>
+      <div>
+        <h2>{title}</h2>
+        {children}
+      </div>
+      <button>Delete</button>
+    </article>
+  );
+}
